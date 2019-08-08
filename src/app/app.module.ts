2f4e7a8922b8b03d73f7ff51aebc5291225d7ae1
Joshua Proffitt <joshua.m.proffitt@gmail.com>
2019-08-08 15:45:29 -0400

    organizationcreate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 
-import { MatToolbarModule } from '@angular/material';
-
+import { MatToolbarModule, MatFormFieldModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -48,6 +48,8 @@ import { UserService } from './services/user.service';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule
--- src/app/services/organization.service.ts
+++ src/app/services/organization.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Organization } from '../models/Organization';
+
+const ApiUrl = 'https://feedtheneedwebapi20190807040544.azurewebsites.net/';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class OrganizationService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  createOrganization(organization: Organization) {
+    return this._http.post(`${ApiUrl}/Organization`, organization, { headers: this.getHeaders() });
+  }
+  
+  private getHeaders() {
+    return new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('id_token')}`)
+  }
 }
