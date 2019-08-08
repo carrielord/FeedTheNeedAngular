0596a0542c6cfccacca395f3137879f9cdf538d5
carrielord <carrie.m.lord@gmail.com>
2019-08-08 13:31:08 -0400

    Fixed service names

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class OrganizationserviceService {
+export class AuthService {
 
   constructor() { }
 }
--- src/app/services/posting.service.ts
+++ src/app/services/posting.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class UserserviceService {
+export class PostingService {
 
   constructor() { }
 }
