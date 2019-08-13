a38461395417a7f7f85c07e8f90211997b94b02f
carrielord <carrie.m.lord@gmail.com>
2019-08-13 16:02:23 -0400

    Updated Login TS

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,33 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import{AuthService} from '../../services/auth.service'
 import{FormGroup, FormControl, FormBuilder} from '@angular/forms';
-
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+ selector: 'app-login',
+ templateUrl: './login.component.html',
+ styleUrls: ['./login.component.css']
 })
-
 export class LoginComponent implements OnInit {
-
-  public loginForm:FormGroup;
-
-  constructor(private _form: FormBuilder, private authService: AuthService) {
-    this.createForm();
-   }
-
-  ngOnInit() {
+ public loginForm:FormGroup;
+ constructor(private _form: FormBuilder, private authService: AuthService) {
+   this.createForm();
   }
-  createForm(){
-    this.loginForm = this._form.group({
-      email: new FormControl,
-      password: new FormControl
-
-    })
-  }
-
-  onSubmit(){
-    this.authService.login(this.loginForm.value);
-  }
-
+ ngOnInit() {
+ }
+ createForm(){
+   this.loginForm = this._form.group({
+     email: new FormControl(),
+     password: new FormControl()
+   })
+ }
+ onSubmit(){
+   this.authService.login(this.loginForm.value);
+   console.log(this.loginForm.value);
+ }
 }
