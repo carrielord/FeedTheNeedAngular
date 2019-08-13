22b2649c552633af187c6cae048aecb8f8c74670
Joshua Proffitt <joshua.m.proffitt@gmail.com>
2019-08-13 17:08:25 -0400

    styling changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './component/header/header.component';
-import { MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatTableModule, MatCardModule, MatGridListModule} from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatTableModule } from '@angular/material';
 import { RouterModule} from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -74,9 +74,8 @@ const routes = [
     MatFormFieldModule,
     MatInputModule,
     MatTableModule,
-    MatCardModule,
-    MatGridListModule,
-    AppRoutingModule
+    MatFormFieldModule,
+    AppRoutingModule,
   ],
 
   providers: [
