69e0c6c5684b3d6886e0844d8d0e722e7b825384
Joshua Proffitt <joshua.m.proffitt@gmail.com>
2019-08-13 14:57:36 -0400

    styling to contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './component/header/header.component';
-import { MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatTableModule, MatCardModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatTableModule, MatCardModule, MatGridListModule} from '@angular/material';
 import { RouterModule} from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -75,6 +75,7 @@ const routes = [
     MatInputModule,
     MatTableModule,
     MatCardModule,
+    MatGridListModule,
     AppRoutingModule
   ],
 
