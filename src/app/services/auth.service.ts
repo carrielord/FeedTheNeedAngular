efc10337525d9d50c0d22b05d86af3692f691245
Joshua Proffitt <joshua.m.proffitt@gmail.com>
2019-08-16 10:59:47 -0400

    first commit test

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,7 +4,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Token } from '../models/Token';
 import{Router} from '@angular/router';
 import{Observable, Subject} from 'rxjs';
-const Api_Url = 'https://localhost:44381';
+import { APIURL } from '../../environments/environment.prod';
+// const APIURL = 'https://localhost:44381';
 @Injectable()
 export class AuthService{
   
@@ -13,24 +14,24 @@ isLoggedIn = new Subject<boolean>();
   constructor(private _http: HttpClient, private _router: Router){}
   
   register(regUserData: RegisterUser){
-    return this._http.post(`${Api_Url}/api/Account/Register`, regUserData);
+    return this._http.post(`${APIURL}/api/Account/Register`, regUserData);
   }
   login(loginInfo){
     
     const str = `grant_type=password&username=${encodeURI(loginInfo.email)}&password=${encodeURI(loginInfo.password)}`;
-    return this._http.post(`${Api_Url}/Token`, str).subscribe((token: Token)=>{
+    return this._http.post(`${APIURL}/Token`, str).subscribe((token: Token)=>{
       localStorage.setItem('id_token', token.access_token);
       this._router.navigate(['/userdetails']);
     });
   }
   currentUser(): Observable<Object>{
     if(!localStorage.getItem('id_token')){return new Observable(observer => observer.next(false));}
-    return this._http.get(`${Api_Url}/api/Account/Userdetails`, {headers: this.setHeader()});
+    return this._http.get(`${APIURL}/api/Account/Userdetails`, {headers: this.setHeader()});
   }
   logout(): Observable<Object>{
     localStorage.clear();
     this.isLoggedIn.next(false);
-    return this._http.post(`$Api_Url}/api/Account/**`, { headers: this.setHeader()});
+    return this._http.post(`$APIURL}/api/Account/**`, { headers: this.setHeader()});
   }
   private setHeader(): HttpHeaders{
     return new HttpHeaders().set('Authorization', `Bearer $(localStorage.getItem('id_token')}`);
